adf2feadd6cec57043b5b9283bae7273794e5a07
Dan Gamble <dangamble89@gmail.com>
2015-07-18 14:21:29 +0100

    Added base list and detail views

--- fifa/urls.py
+++ fifa/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -15,7 +16,7 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
 
     # Nations
-    url(r'^/nations/', include('fifa.apps.nations.urls', namespace='nations')),
+    url(r'^nations/', include('fifa.apps.nations.urls', namespace='nations')),
 )
 
 if settings.DEBUG:
