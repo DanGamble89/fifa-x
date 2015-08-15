6a4d53b04d8d0cf0d9dd3dc5575d259db1d79c19
Dan Gamble <dangamble89@gmail.com>
2015-08-15 16:59:16 +0100

    Added django defaultfilters to Jinja templates

--- fifa/jinja2.py
+++ fifa/jinja2.py
@@ -1,6 +1,8 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.core.urlresolvers import reverse
+from django.template import defaultfilters
+
 from jinja2 import Environment
+
 from fifa.apps.players.views import build_url
 
 
@@ -10,7 +12,8 @@ def environment(**options):
     env.globals.update({
         'static': staticfiles_storage.url,
         # 'url': reverse,
-        'url': build_url
+        'url': build_url,
+        'dj': defaultfilters
     })
 
     return env
