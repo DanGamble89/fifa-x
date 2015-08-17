1d58520e28419ee7a7ef490e009a48aaf6e0fb6f
Dan Gamble <dangamble89@gmail.com>
2015-08-17 21:57:09 +0100

    Added an imperial height Jinja filter

--- fifa/jinja2.py
+++ fifa/jinja2.py
@@ -2,12 +2,27 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from django.template import defaultfilters
 
 from jinja2 import Environment
+import math
 
 from fifa.apps.players.views import build_url
 
 
+def height_imperial(height, labels=False):
+    inches, feet = math.modf(height * 0.032808399)
+
+    value = '{}{feet}{}{inches}'.format(
+        int(feet), int(round(inches * 12, 1)),
+        feet='ft ' if labels else '\'',
+        inches='in' if labels else '"'
+    )
+
+    return value
+
+
 def environment(**options):
     env = Environment(**options)
+
+    env.filters['height_imperial'] = height_imperial
 
     env.globals.update({
         'static': staticfiles_storage.url,
